c1c83004fcf09e1db157ba8ca5ade7f6bbafaec5
itskh4ng <itskh4ng@gmail.com>
2024-09-05 16:31:06 -0700

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,3 +1,2 @@
-
-javascript:(function() { var win = window.open('', '', 'fullscreen=yes'); var url = "https://noir-engine.pages.dev"; var iframe = win.document.createElement('iframe'); iframe.style.width = "100vw"; iframe.style.height = "100vh"; iframe.style.border = "none"; iframe.src = url; win.document.body.style.margin = "0"; win.document.body.appendChild(iframe); })();
+(function() { var win = window.open('', '', 'fullscreen=yes'); var url = "https://noir-engine.pages.dev"; var iframe = win.document.createElement('iframe'); iframe.style.width = "100vw"; iframe.style.height = "100vh"; iframe.style.border = "none"; iframe.src = url; win.document.body.style.margin = "0"; win.document.body.appendChild(iframe); })();
 
