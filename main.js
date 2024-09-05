5a31f9e907401a9bb0855e760e2bc20f91cda52f
itskh4ng <itskh4ng@gmail.com>
2024-09-05 16:43:25 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-javascript:(function(){
-  fetch('https://raw.githubusercontent.com/itskh4ng/noir/main/engine.js')
-    .then(response => response.text())
-    .then(scriptContent => {
-      eval(scriptContent);
-    })
-    .catch(error => console.error('Error loading script:', error));
-})();
+
